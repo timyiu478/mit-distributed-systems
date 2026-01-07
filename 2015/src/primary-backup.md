fcc38f0c259f3abcb44908d6f8e7da57625d23bc
Kam Wing Yiu <ctom3560@gmail.com>
2026-01-07 13:20:57 +0800

    view service: progress

---
+++ 2015/src/primary-backup.md
@@ -0,0 +1,19 @@
+# Primary/Backup Key/Value Service
+
+## Split Brain Prevention
+
+The primary must send Gets as well as Puts to the backup (if there is one), and must wait for the backup to reply before responding to the client. This helps prevent two servers from acting as primary (a "split brain"). An example: S1 is the primary and S2 is the backup. The view service decides (incorrectly) that S1 is dead, and promotes S2 to be primary. If a client thinks S1 is still the primary and sends it an operation, S1 will forward the operation to S2, and S2 will reply with an error indicating that it is no longer the backup (assuming S2 obtained the new view from the viewservice). S1 can then return an error to the client indicating that S1 might no longer be the primary (reasoning that, since S2 rejected the operation, a new view must have been formed); the client can then ask the view service for the correct primary (S2) and send it the operation.
+
+## Fault-tolerance and Performance Limitations 
+
+* The view service is vulnerable to failures, since it's not replicated.
+* The primary and backup must process operations one at a time, limiting their performance.
+* A recovering server must copy a complete database of key/value pairs from the primary, which will be slow, even if the recovering server has an almost-up-to-date copy of the data already (e.g. only missed a few minutes of updates while its network connection was temporarily broken).
+* The servers don't store the key/value database on disk, so they can't survive simultaneous crashes (e.g., a site-wide power failure).
+* If a temporary problem prevents primary to backup communication, the system has only two remedies: change the view to eliminate the backup, or keep trying; neither performs well if such problems are frequent.
+* If a primary fails before acknowledging the view in which it is primary, the view service cannot make progress---it will spin forever and not perform a view change.
+
+
+## Details
+
+http://nil.csail.mit.edu/6.824/2015/labs/lab-2.html
--- 2015/src/viewservice/server.go
+++ 2015/src/viewservice/server.go
@@ -16,8 +16,22 @@ type ViewServer struct {
 	rpccount int32 // for testing
 	me       string
 
-
 	// Your declarations here.
+	// keep track of the most recent time at which the viewservice has heard a Ping from each server
+	lastPings map[string]time.Time
+	// Keep track of the real-time status of primary/backup
+	members   map[string]bool // false -> restarted/failed
+	// Keep track of the return view
+	viewnum     uint
+	primary     string
+	backup      string
+	primaryLive bool
+	backupLive  bool
+	// keep track of whether the primary for the current view has acknowledged it
+	primaryAcked bool
+
+	
+
 }
 
 //
@@ -26,6 +40,30 @@ type ViewServer struct {
 func (vs *ViewServer) Ping(args *PingArgs, reply *PingReply) error {
 
 	// Your code here.
+
+	// The primary for the current view has acknowledged it
+	if vs.viewnum == args.Viewnum && args.Me == vs.primary || (vs.viewnum == 0) {
+		vs.primaryAcked = true
+		if vs.viewnum == 0 {
+			vs.viewnum = args.Viewnum
+		}
+	}
+
+	// Update last ping time
+	vs.lastPings[args.Me] = time.Now()
+	vs.members[args.Me] = true
+
+	// Viewnum = 0 represents the server has failed and re-started
+	if args.Viewnum == 0 {
+		vs.members[args.Me] = false
+		log.Println("Server", args.Me, " has failed and re-started")
+	}
+
+	vs.changeView()
+
+	reply.View.Viewnum = vs.viewnum
+	reply.View.Primary = vs.primary
+	reply.View.Backup = vs.backup
 
 	return nil
 }
@@ -36,6 +74,12 @@ func (vs *ViewServer) Ping(args *PingArgs, reply *PingReply) error {
 func (vs *ViewServer) Get(args *GetArgs, reply *GetReply) error {
 
 	// Your code here.
+	vs.mu.Lock()
+	defer vs.mu.Unlock()
+
+	reply.View.Viewnum = vs.viewnum
+	reply.View.Primary = vs.primary
+	reply.View.Backup = vs.backup
 
 	return nil
 }
@@ -49,6 +93,19 @@ func (vs *ViewServer) Get(args *GetArgs, reply *GetReply) error {
 func (vs *ViewServer) tick() {
 
 	// Your code here.
+	vs.mu.Lock()
+	defer vs.mu.Unlock()
+
+	for server, lastPing := range vs.lastPings {
+		vs.members[server] = true
+		if lastPing.Add(PingInterval).Before(time.Now()) {
+			delete(vs.lastPings, server)
+			vs.members[server] = false
+			log.Println("Server", server, "has no ping")
+		}
+	}
+
+	vs.changeView()
 }
 
 //
@@ -77,6 +134,12 @@ func StartServer(me string) *ViewServer {
 	vs := new(ViewServer)
 	vs.me = me
 	// Your vs.* initializations here.
+	vs.lastPings = make(map[string]time.Time)
+	vs.members = make(map[string]bool)
+	vs.primaryAcked = false
+	vs.primary = ""
+	vs.backup = ""
+	vs.members[""] = false
 
 	// tell net/rpc about our RPC server and handlers.
 	rpcs := rpc.NewServer()
@@ -120,4 +183,74 @@ func StartServer(me string) *ViewServer {
 	}()
 
 	return vs
+}
+
+//
+// Change view if primary is acknowledged and the primary/backupstatus can change 
+//
+func (vs *ViewServer) changeView() {
+	// No ACK from primary
+	if !vs.primaryAcked {
+		return
+	}
+
+	// Count # of failures
+	failCount := 0
+
+	if !vs.members[vs.primary] {
+		failCount += 1
+	}
+	if !vs.members[vs.backup] {
+		failCount += 1
+	}
+
+	// Primary and backup are health
+	if failCount == 0 {
+		return
+	}
+
+	viewChanged := false
+
+  if !vs.members[vs.primary] {
+		// Promote backup as primary
+		if vs.members[vs.backup] {
+			vs.primary = vs.backup
+			vs.members[vs.primary] = true
+			vs.members[vs.backup] = false
+			vs.backup = ""
+			viewChanged = true
+		} else if len(vs.lastPings) > 0 {
+			// Select an idle(restarted) server as primary
+			for server, _ := range vs.lastPings {
+				vs.primary = server
+				vs.members[vs.primary] = true
+				break
+			}
+			viewChanged = true
+		} else if vs.primary != "" {
+			vs.primary = ""
+			viewChanged = true
+		}
+	}
+
+	if !vs.members[vs.backup] {
+		// Select an idle(restarted) server as primary
+		if len(vs.lastPings) > 1 {
+			for server, _ := range vs.lastPings {
+				if server == vs.primary { continue }
+				vs.backup = server
+				vs.members[vs.backup] = true
+				viewChanged = true
+				break
+			}
+		} else if vs.backup != "" {
+			vs.backup = ""
+			viewChanged = true
+		}
+	}
+
+	if viewChanged {
+		vs.viewnum += 1
+		vs.primaryAcked = false
+	}
 }
