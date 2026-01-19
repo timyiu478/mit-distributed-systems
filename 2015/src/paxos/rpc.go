465ac3db5372a173bc0138c1521ca083bf08cf3d
Kam Wing Yiu <ctom3560@gmail.com>
2026-01-19 18:19:16 +0800

    add rpc

---
+++ 2015/src/paxos/rpc.go
@@ -0,0 +1,65 @@
+package paxos
+
+const (
+	OK             = "OK"
+	ErrReject      = "ErrReject"
+)
+
+type Err string
+
+type Proposal struct {
+	Num       int
+	Id        int
+}
+
+func (p1 Proposal) lessThan(p2 Proposal) bool {
+	if p1.Num < p2.Num { return true }
+	if p1.Num > p2.Num { return false }
+	if p1.Id < p2.Id { return true }
+
+	return false
+}
+
+type PrepareArgs struct {
+	Me  		  int
+	SeqNum  	int  // Instance Sequence Number
+	DoneSeq   int  // Highest Done argument supplied by its local application
+	Num  			Proposal
+}
+
+type PrepareReply struct {
+	Me  		int
+	SeqNum 	int
+	DoneSeq int
+	Num     Proposal
+	Va   		interface{} // Value Accepted
+	Err 		Err
+}
+
+type DecidedArgs struct {
+	Me  		int
+	SeqNum 	int
+	DoneSeq int
+}
+
+type DecidedReply struct {
+	Me  		int
+	SeqNum 	int
+	DoneSeq int
+}
+
+
+type AcceptArgs struct {
+	Me  		int
+	SeqNum 	int
+	DoneSeq int
+	Num     Proposal
+	Value   interface{}
+}
+
+type AcceptReply struct {
+	Me  		int
+	SeqNum 	int
+	DoneSeq int
+	Err 		Err
+}
